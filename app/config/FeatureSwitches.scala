2c74dbc4920383d766e58043d476131ef11889fc
GazalNebHMRC <108878679+GazalNebHMRC@users.noreply.github.com>
2023-01-11 15:23:27 +0000

    MTDSA-14344: corrected auto-merging mistake

--- app/config/FeatureSwitches.scala
+++ app/config/FeatureSwitches.scala
@@ -50,14 +50,6 @@ case class FeatureSwitches(featureSwitchConfig: Configuration) {
     }
   }
 
-  def isTemporalValidationEnabled(implicit request: Request[_]): Boolean = {
-    if (isEnabled("allowTemporalValidationSuspension.enabled")) {
-      request.headers.get("suspend-temporal-validations").forall(!BooleanUtils.toBoolean(_))
-    } else {
-      true
-    }
-  }
-
   private def isEnabled(key: String): Boolean = featureSwitchConfig.getOptional[Boolean](key).getOrElse(true)
 }
 
